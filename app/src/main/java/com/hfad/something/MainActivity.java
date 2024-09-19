30e765ad4da5bf6cfeb14be7a0acbd8d4b3e54e1
Hung1609 <phanlachung2004@gmail.com>
2024-09-19 13:42:36 +0700

    update page statis and image

--- app/src/main/java/com/hfad/something/MainActivity.java
+++ app/src/main/java/com/hfad/something/MainActivity.java
@@ -1,26 +1,42 @@
 package com.hfad.something;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import android.view.MenuItem;
+import android.widget.Toast;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.widget.Toolbar;
 
-import com.google.android.material.tabs.TabLayout;
+public class MainActivity extends AppCompatActivity{
 
-public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //Drawer and navigationview
+        drawerLayout = findViewById(R.id.main);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.open_nav, R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
         // Create and add StatsFragment
         StatsFragment statsFragment = new StatsFragment();
         getSupportFragmentManager().beginTransaction().add(R.id.main, statsFragment).commit();
@@ -32,5 +48,7 @@ public class MainActivity extends AppCompatActivity {
         // Create and set TabLayout
         TabLayout tabLayout = findViewById(R.id.tabLayout);
         tabLayout.setupWithViewPager(viewPager);
+
+        }
     }
 }
