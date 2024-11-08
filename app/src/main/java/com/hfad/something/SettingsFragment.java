11e22fa112a968a7c3d1a38b9ee035fa3766875f
duy1516 <duynguyenhoang1611@gmail.com>
2024-11-09 01:17:55 +0700

    profile

--- app/src/main/java/com/hfad/something/SettingsFragment.java
+++ app/src/main/java/com/hfad/something/SettingsFragment.java
@@ -1,14 +1,24 @@
 package com.hfad.something;
 
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Switch;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class SettingsFragment extends Fragment {
 
@@ -16,8 +26,11 @@ public class SettingsFragment extends Fragment {
         // Required empty public constructor
     }
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_settings, container, false);
     }
 }
+
